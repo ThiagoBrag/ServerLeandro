2e378776d728b459ab0a309615fe7e59b8b36152
ThiagoBrag <92130098+ThiagoBrag@users.noreply.github.com>
2022-06-22 21:57:39 -0300

    a
    a

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const cors = require('cors');
 const server = express();
 const port = 3000;
 
-const { Sequelize } = require('sequelize')
+server.use(cors());
+server.use(express.json());
+
+const { Sequelize, DataTypes } = require('sequelize')
 
 const sequelize = new Sequelize('leandro', 'root', 'root', {
     host: 'localhost',
@@ -16,13 +19,31 @@ sequelize.authenticate().then(() => {
     console.log('DEU ERRO: ', err);
 });
 
-server.use(cors());
-server.use(express.json());
-
-server.get('/', (req, res) => {
-    res.json({message: '/Home'})
+const User = sequelize.define('users', {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    sobrenome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    createdAt: false,
+    updatedAt: false
 });
 
-server.listen(port, () => {
+server.post('/user', (req, res) => {
+    const { nome, sobrenome } = req.body;
+
+    User.create({nome, sobrenome}).then(() => {
+        res.json({message: "Success"});
+    }).catch(err => {
+        res.json({err: "Error"})
+    });
+});
+
+server.listen(port, async () => {
+    await sequelize.sync();
     console.log(`Escutando na porta http://localhost:${port}`)
 });
